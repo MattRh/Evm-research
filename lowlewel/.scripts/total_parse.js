7f175bee8fe038ea6b33f28a5bba2bb02f22684b
Sergey <sergejrf@gmail.com>
2018-06-16 15:03:24 +0300

    Update

--- lowlewel/.scripts/total_parse.js
+++ lowlewel/.scripts/total_parse.js
@@ -3,6 +3,9 @@
 require("./parsers/solidity_parse");
 require("./parsers/solidity_comments_parse");
 
-require("./parsers/manticore_parse");
+require("./parsers/ethereum_parse");
+require("./parsers/ethereum_comments_parse");
 
-require("./check_consistency");
+require("./parsers/manticore_parse");
+require("./parsers/gas_costs_parse");
+require("./parsers/assembly_parse");
